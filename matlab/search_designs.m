7d66a11cbea872ec2584d1257371bad788784123
Alex Elzenaar <aelz176@aucklanduni.ac.nz>
2020-01-11 21:12:38 +1300

    Clean up search_designs
     - No longer sets own directory, so can be killed in MATLAB without
       messing up the user's directory
     - Logging now all goes to log.txt in output dir

--- matlab/search_designs.m
+++ matlab/search_designs.m
@@ -4,22 +4,21 @@
 t = 3;
 d = 2;
 n_min = 1;
-n_max = 3;
+n_max = 10;
 
 comment = 'search_designs';
 
 s = 1000; % Number of seeds to try first
 fast_k = 1000; % Number of times to iterate to find a good initial step size
-slow_k = 1000; % Number of iterations for the proper iteration.
+slow_k = 1e7; % Number of iterations for the proper iteration.
 
 dirname = sprintf('search_designs_%d_%d_%s',d,t,datestr(datetime('now'),'yyyy-mm-dd-HH-MM-SS'));
 mkdir(dirname);
-chdir(dirname);
-log_file = fopen('log.txt','w');
+log_file = fopen(sprintf('%s/log.txt', dirname),'w');
 
 for n = n_min:n_max
     errorComputer = ComplexDesignPotential(d,n,t);
-    seed = getRandomComplexSeed(d,n,s,errorComputer,1);
+    seed = getRandomComplexSeed(d,n,s,errorComputer,log_file);
     initialError = errorComputer.computeError(seed);
     fprintf(1, '[n = %d] Seed has error %E.\n',n,initialError);
     r = NaN;
@@ -44,15 +43,14 @@ for n = n_min:n_max
         fprintf(1, '[n = %d] Found good step size. Iterating... ',n);
         [result, errors, ~] = iterateOnDesign(result, slow_k, 2, 10^(-r), 1, errorComputer, log_file);
         error = errors(end);
-        fprintf(1, 'done with final error %E\n',n);
+        fprintf(1, 'done with final error %E\n',error);
     end
     
     errorMultiplier = 10^(-r);
-    save(sprintf('run_%d.mat',n), 'result','errors','d','n','t','errorMultiplier','comment');
+    save(sprintf('%s/run_%d.mat',dirname,n), 'result','errors','d','n','t','errorMultiplier','comment');
     plot(1:length(errors),errors);
     set(gca, 'YScale', 'log');
-    saveas(gcf, sprintf('run_%d_errors.png',n));
+    saveas(gcf, sprintf('%s/run_%d_errors.png',dirname,n));
     close(gcf);
 end
 
-chdir('..')
